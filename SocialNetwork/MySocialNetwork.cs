00bc5115f7796010380006ff5582a1cade4f93b8
jvaquero <jvaquero@aidacanarias.com>
2022-05-19 08:00:18 +0100

    feat user posts a message

--- SocialNetwork/MySocialNetwork.cs
+++ SocialNetwork/MySocialNetwork.cs
@@ -2,14 +2,22 @@ namespace SocialNetwork;
 
 public class MySocialNetwork
 {
+    public ICollection<User> Users { get; }
+
+    public MySocialNetwork()
+    {
+        Users = new List<User>();
+    }
+
     public void AddUser(string userName)
     {
-        throw new NotImplementedException();
+        Users.Add(new User(userName));
     }
-    
+
+
     public void CreatePost(string user, string message)
     {
-        throw new NotImplementedException();
+        Users.Single(u => u.UserName.Equals(user)).Posts.Add(new Post(message));
     }
 
     public void Follow(string user, string userToFollow)
@@ -20,5 +28,27 @@ public class MySocialNetwork
     public ICollection<string> GetSubscriptionsMessages(string user)
     {
         throw new NotImplementedException();
+    }
+}
+
+public class User
+{
+    public string UserName { get; }
+    public ICollection<Post> Posts { get; set; }
+
+    public User(string userName)
+    {
+        UserName = userName;
+        Posts = new List<Post>();
+    }
+}
+
+public class Post
+{
+    public string Message { get; }
+
+    public Post(string message)
+    {
+        Message = message;
     }
 }
