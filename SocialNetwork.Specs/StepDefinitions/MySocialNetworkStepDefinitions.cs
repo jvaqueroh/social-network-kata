284709b32358cf4ae563ec9d4974835d6a5817bb
jvaquero <jvaquero@aidacanarias.com>
2022-05-19 07:57:35 +0100

    implement step definitions

--- SocialNetwork.Specs/StepDefinitions/MySocialNetworkStepDefinitions.cs
+++ SocialNetwork.Specs/StepDefinitions/MySocialNetworkStepDefinitions.cs
@@ -3,42 +3,41 @@ namespace SocialNetwork.Specs.StepDefinitions
     [Binding]
     public sealed class MySocialNetworkStepDefinitions
     {
+        private readonly MySocialNetwork mySocialNetwork;
+        private ICollection<string> subscriptionsMessages;
+
         // For additional details on SpecFlow step definitions see https://go.specflow.org/doc-stepdef
+        public MySocialNetworkStepDefinitions(MySocialNetwork mySocialNetwork)
+        {
+            this.mySocialNetwork = mySocialNetwork;
+        }
 
         [Given("user (.*) posts the message (.*)")]
         public void GivenUserPostsAMessage(string user, string message)
         {
-            //TODO: implement arrange (precondition) logic
-            // For storing and retrieving scenario-specific data see https://go.specflow.org/doc-sharingdata
-            // To use the multiline text or the table argument of the scenario,
-            // additional string/Table parameters can be defined on the step definition
-            // method. 
-
-            throw new PendingStepException();
+            mySocialNetwork.AddUser(user);
+            mySocialNetwork.CreatePost(user, message);
         }
 
         [Given("user (.*) follows users (.*)")]
         public void GivenUserFollowsOtherUsers(string user, string followedUsers)
         {
-            //TODO: implement arrange (precondition) logic
-
-            throw new PendingStepException();
+            foreach (var userToFollow in followedUsers.Split(","))
+            {
+                mySocialNetwork.Follow(user, userToFollow);
+            }
         }
 
         [When("user (.*) gets the subscriptions")]
         public void WhenUserGetsTheSubscriptions(string user)
         {
-            //TODO: implement act (action) logic
-
-            throw new PendingStepException();
+            subscriptionsMessages = mySocialNetwork.GetSubscriptionsMessages(user);
         }
 
         [Then("subscriptions list of user (.*) contains (.*)")]
         public void ThenUserSubscriptionsContains(string user, string message)
         {
-            //TODO: implement assert (verification) logic
-
-            throw new PendingStepException();
+            subscriptionsMessages.Should().Contain(message);
         }
     }
 }
---
+++ SocialNetwork/MySocialNetwork.cs
@@ -0,0 +1,24 @@
+namespace SocialNetwork;
+
+public class MySocialNetwork
+{
+    public void AddUser(string userName)
+    {
+        throw new NotImplementedException();
+    }
+    
+    public void CreatePost(string user, string message)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Follow(string user, string userToFollow)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ICollection<string> GetSubscriptionsMessages(string user)
+    {
+        throw new NotImplementedException();
+    }
+}
