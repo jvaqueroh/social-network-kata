596590a1b69440db0cdce418b4dbd56f8c380416
jvaquero <jvaquero@aidacanarias.com>
2022-06-08 08:07:04 +0100

    feat: save_a_message_posted_by_a_registered_user

--- SocialNetwork/MySocialNetwork.cs
+++ SocialNetwork/MySocialNetwork.cs
@@ -5,10 +5,12 @@ public class MySocialNetwork
     public void AddUser(User user)
     {
         Database.Users.Add(user);
+        Database.Posts.Add(user, new List<string>());
     }
 
     public void Post(User user, string message)
     {
+        Database.Posts[user].Add(message);
     }
 
     public ICollection<string> GetTimeline(User readerUser, User timelineOwnerUser)
