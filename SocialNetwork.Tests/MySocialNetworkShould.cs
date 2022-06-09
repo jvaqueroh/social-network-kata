1356655ef3eb838503237d42e63734714e6c4d08
jvaquero <jvaquero@aidacanarias.com>
2022-06-09 07:28:03 +0100

    refactor[unit-tests]: extract common given methods

--- SocialNetwork.Tests/MySocialNetworkShould.cs
+++ SocialNetwork.Tests/MySocialNetworkShould.cs
@@ -11,11 +11,18 @@ namespace SocialNetwork.Tests
 {
     public class MySocialNetworkShould
     {
+        private MySocialNetwork mySocialNetwork;
+
+        [SetUp]
+        public void SetUp()
+        {
+            mySocialNetwork = new MySocialNetwork();
+        }
+
         [Test]
         public void register_a_user()
         {
             User newUser = new User("Bob");
-            var mySocialNetwork = new MySocialNetwork();
 
             mySocialNetwork.AddUser(newUser);
 
@@ -25,9 +32,7 @@ namespace SocialNetwork.Tests
         [Test]
         public void save_a_message_posted_by_a_registered_user()
         {
-            User givenUser = new User("Alice");
-            var mySocialNetwork = new MySocialNetwork();
-            mySocialNetwork.AddUser(givenUser);
+            User givenUser = GivenRegisterdUser("Alice");
 
             var message = "Hi! I'm Alice.";
             mySocialNetwork.Post(givenUser, message);
@@ -38,20 +43,28 @@ namespace SocialNetwork.Tests
         [Test]
         public void get_the_timeline_of_a_registered_user_for_another_user()
         {
-            var mySocialNetwork = new MySocialNetwork();
-            var readerUser = new User("Bob");
-            mySocialNetwork.AddUser(readerUser);
-            var timelineUser = new User("Alice");
-            mySocialNetwork.AddUser(timelineUser);
-            var firstPost = "Hi! I'm Alice.";
-            mySocialNetwork.Post(timelineUser, firstPost);
-            var secondPost = "This is my timeline.";
-            mySocialNetwork.Post(timelineUser, secondPost);
+            var readerUser = GivenRegisterdUser("Bob");
+            var timelineUser = GivenRegisterdUser("Alice");
+            var firstPost = GivenRegisteredUserPostsAMessage(timelineUser, "Hi! I'm Alice.");
+            var secondPost = GivenRegisteredUserPostsAMessage(timelineUser, "This is my timeline.");
             var expectedMessages = new[] { firstPost, secondPost };
 
             var result = mySocialNetwork.GetTimeline(readerUser, timelineUser);
-            
+
             result.Should().BeEquivalentTo(expectedMessages);
+        }
+
+        private string GivenRegisteredUserPostsAMessage(User timelineUser, string message)
+        {
+            mySocialNetwork.Post(timelineUser, message);
+            return message;
+        }
+
+        private User GivenRegisterdUser(string userName)
+        {
+            var readerUser = new User(userName);
+            mySocialNetwork.AddUser(readerUser);
+            return readerUser;
         }
     }
 }
