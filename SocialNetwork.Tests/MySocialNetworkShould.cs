b2d73d615941e49c52a2663f5b1a9a788b95d7e9
jvaquero <jvaquero@aidacanarias.com>
2022-06-09 07:16:06 +0100

    test: get_the_timeline_of_a_registered_user_for_another_user

--- SocialNetwork.Tests/MySocialNetworkShould.cs
+++ SocialNetwork.Tests/MySocialNetworkShould.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -32,6 +33,25 @@ namespace SocialNetwork.Tests
             mySocialNetwork.Post(givenUser, message);
 
             Database.Posts[givenUser].Should().Contain(message);
+        }
+
+        [Test]
+        public void get_the_timeline_of_a_registered_user_for_another_user()
+        {
+            var mySocialNetwork = new MySocialNetwork();
+            var readerUser = new User("Bob");
+            mySocialNetwork.AddUser(readerUser);
+            var timelineUser = new User("Alice");
+            mySocialNetwork.AddUser(timelineUser);
+            var firstPost = "Hi! I'm Alice.";
+            mySocialNetwork.Post(timelineUser, firstPost);
+            var secondPost = "This is my timeline.";
+            mySocialNetwork.Post(timelineUser, secondPost);
+            var expectedMessages = new[] { firstPost, secondPost };
+
+            var result = mySocialNetwork.GetTimeline(readerUser, timelineUser);
+            
+            result.Should().BeEquivalentTo(expectedMessages);
         }
     }
 }
