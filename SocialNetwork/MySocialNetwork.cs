94148fedc8f030423581301fc61d1fe38693036c
jvaquero <jvaquero@aidacanarias.com>
2022-06-15 07:45:06 +0100

    feat: save_a_private_message_from_an_user_to_another_user with FromUser

--- SocialNetwork/MySocialNetwork.cs
+++ SocialNetwork/MySocialNetwork.cs
@@ -51,7 +51,7 @@ public class MySocialNetwork
 
     public void SendPrivateMessage(User senderUser, User receiverUser, string message)
     {
-        Database.PrivateMessages[receiverUser].Add(PrivateMessage.Create(message, null));
+        Database.PrivateMessages[receiverUser].Add(PrivateMessage.Create(message, senderUser));
     }
 
     public ICollection<string> GetPrivateMessages(User user)
